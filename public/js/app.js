a579284e70f8060866b664e16ce77eb102ef93dc
aymen abbad andaloussi <aymenandaloussi69@gmail.com>
2025-05-27 22:31:46 +0100

    end menu

---
+++ public/js/app.js
@@ -0,0 +1,34 @@
+//^ carouselle testimonial
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+
+let content = document.getElementById("content");
+let item1 = document.querySelector("#item1");
+let item2 = document.querySelector("#item2");
+let item3 = document.querySelector("#item3");
+
+if (item1.style.display == "flex") {
+  button1.addEventListener("click", () => {
+    item1.style.display = "flex";
+    item2.style.display = "none";
+    item3.style.display = "none";
+  });
+}
+
+if (item2.style.display == "none") {
+  button2.addEventListener("click", () => {
+    item2.style.display = "flex";
+    item1.style.display = "none";
+    item3.style.display = "none";
+  });
+}
+
+if (item3.style.display == "none") {
+  button3.addEventListener("click", () => {
+    item3.style.display = "flex";
+    item2.style.display = "none";
+    item1.style.display = "none";
+  });
+}
+//^ carouselle testimonial
